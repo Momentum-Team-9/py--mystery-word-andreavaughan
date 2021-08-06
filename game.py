80ff5156dc06bfead229c6810230b20df0e1f656
andreavaughan <amvaughan16@gmail.com>
2021-08-06 14:39:57 -0400

    building out advanced game logic

--- game.py
+++ game.py
@@ -16,9 +16,22 @@ user_input = input('Guess a letter: ')
 
 guess_count = 1 
 
-while user_input != 'Quit':
-    letter_guesses = ['e', 't']
+letter_guesses = []
 
+
+def display_word(letter, guesses):
+    if letter in guesses:
+        return letter
+    else: 
+        return "_"
+
+
+def print_word(word, guesses):
+    output_letters = [display_word(letter, letter_guesses) for letter in mystery_word]
+    print(' '.join(output_letters))
+
+
+while user_input != 'Quit':
     if guess_count == 8:
         print('You are out of guesses!')
         break
@@ -27,7 +40,15 @@ while user_input != 'Quit':
         user_input = input('You\'ve already guessed that letter! Try again. ')
 
     elif user_input not in letter_guesses and user_input in mystery_word:
-        user_input = input('Correct! Make your next guess. ')
+        letter_guesses.append(user_input)
+        guesses_display = print_word(mystery_word, letter_guesses)
+        
+        if mystery_word == guesses_display:
+            print('You won!')
+
+        else:
+            user_input = input('Correct! Make your next guess. ')
 
     elif user_input not in letter_guesses and user_input not in mystery_word:
         user_input = input('Incorrect! Try again. ')
+        letter_guesses.append(user_input)
