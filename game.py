6040d3a57c8cf31d2d963175e0aadfc0f661b889
andreavaughan <amvaughan16@gmail.com>
2021-08-08 22:21:37 -0400

    added handling for wrong inputs, spacing for command line display

--- game.py
+++ game.py
@@ -46,20 +46,30 @@ def print_word(word, guesses):
 
 first_display = print_word(mystery_word, letter_guesses)
 
+print(' ')
 print(f'Your word has {len(mystery_word)} letters:  ', first_display)
-print(mystery_word)
 
 user_input = input('Guess a letter: ').lower()
 
 while user_input.lower() != 'quit':
     user_input = user_input.lower()
 
-    if user_input in letter_guesses:
+    if len(user_input) >= 2:
+        print(' ')
+        user_input = input('Only guess one letter at a time. Try again. ')
+
+    elif " " in user_input:
+        print(' ')
+        user_input = input('That is not a valid guess. Try again. ')
+
+    elif user_input in letter_guesses:
+        print(' ')
         user_input = input('You\'ve already guessed that letter! Try again. ')
 
     elif user_input not in letter_guesses and user_input in mystery_word:
         letter_guesses.append(user_input)
         guesses_display = print_word(mystery_word, letter_guesses)
+        print(' ')
         print(guesses_display)
                 
         if "_" not in guesses_display:
@@ -72,8 +82,8 @@ while user_input.lower() != 'quit':
                 guess_count = 0 
                 letter_guesses = []
                 first_display = print_word(mystery_word, letter_guesses)
+                print(' ')
                 print(f'Your word has {len(mystery_word)} letters:  ', first_display)
-                print(mystery_word)
 
                 user_input = input('Guess a letter: ')
 
@@ -82,6 +92,7 @@ while user_input.lower() != 'quit':
 
     elif user_input not in letter_guesses and user_input not in mystery_word:
         guess_count += 1
+        print(' ')
 
         if guess_count == 8:
             print('You are out of guesses!')
@@ -94,12 +105,12 @@ while user_input.lower() != 'quit':
                 guess_count = 0 
                 letter_guesses = []
                 first_display = print_word(mystery_word, letter_guesses)
+                print(' ')
                 print(f'Your word has {len(mystery_word)} letters:  ', first_display)
-                print(mystery_word)
 
                 user_input = input('Guess a letter: ')
 
         else:
-            print(f'You\'ve used {guess_count} of 8 incorrect guesses.')
+            print(f'Incorrect! You\'ve used {guess_count} of 8 incorrect guesses.')
             letter_guesses.append(user_input)
-            user_input = input('Incorrect! Try again. ')
+            user_input = input('Try again. ')
